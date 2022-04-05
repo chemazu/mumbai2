2c93e8d66080102d2f953754113c73f770aafa10
chemazu <chemazu>
2022-04-05 12:42:27 +0100

    pop up function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("ge")
+function myFunction() {
+  //   var x = document.getElementsByClassName("close");
+  var x = document.getElementById("close");
+
+  var y = document.getElementById("open");
+
+  console.log(y.style);
+  if (y.style.display === "none") {
+    y.style.display = "flex";
+  } else {
+    y.style.display = "none";
+  }
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
